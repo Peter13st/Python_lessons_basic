5c6628ee73b1aecb8957e5d1fe3086a59f50d9c6
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-11 00:36:41 +0300

    Update hw02_easy.py
    les02hweasy tasks completed, Petr O.

--- lesson02/home_work/hw02_easy.py
+++ lesson02/home_work/hw02_easy.py
@@ -12,14 +12,34 @@
 # 4.  арбуз
 
 # Подсказка: воспользоваться методом .format()
-
+fruits = ["груша", "яблоко", "слива", "банан", "киви", "дыня", "арбуз"]
+i=1
+for fruit in fruits:
+    print('{0:d}.{1:>10}'.format(i, fruit))
+    i+=1
 
 # Задача-2:
 # Даны два произвольные списка.
 # Удалите из первого списка элементы, присутствующие во втором списке.
-
+lst1 = ['q','w','e','r','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+print('list before removing:', lst1)
+lst2 = ['p','k','n','g','r','d','z']
+print('removing these items:', lst2)
+for li in lst1:
+    if li in lst2:
+        lst1.remove(li)
+print('list after removing:', lst1)
 
 # Задача-3:
 # Дан произвольный список из целых чисел.
 # Получите НОВЫЙ список из элементов исходного, выполнив следующие условия:
 # если элемент кратен двум, то разделить его на 4, если не кратен, то умножить на два.
+numint1 = [48,939,23565,23,9948,26,80,13,800,23551,736]
+numint2 = []
+for lnum in numint1:
+    if lnum % 2 == 0:
+        numint2.append(lnum / 4)
+    else:
+        numint2.append(lnum * 2)
+print('first list:', numint1)
+print('second list:', numint2)
