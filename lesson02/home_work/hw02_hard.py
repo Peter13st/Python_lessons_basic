5ba127b81e1737797220023fc9ebff3bae5eab92
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-11 00:31:20 +0300

    Update hw02_hard.py
    hw02hard task 1, 2 completed, Petr O.

--- lesson02/home_work/hw02_hard.py
+++ lesson02/home_work/hw02_hard.py
@@ -4,7 +4,16 @@
 equation = 'y = -12x + 11111140.2121'
 x = 2.5
 # вычислите и выведите y
-
+seq = equation.strip()
+posx = seq.find('x')
+startk = seq.rfind(' ',0,posx)+1
+k = float(seq[startk:posx])
+startb = seq.rfind(' ',posx)+1
+b = float(seq[startb:])
+y = k * x + b
+print(equation)
+print('x = ',x)
+print('y = ',y)
 
 # Задание-2: Дата задана в виде строки формата 'dd.mm.yyyy'.
 # Проверить, корректно ли введена дата.
@@ -17,13 +26,33 @@ x = 2.5
 #  (т.е. 2 символа для дня, 2 - для месяца, 4 - для года)
 
 # Пример корректной даты
-date = '01.11.1985'
+# date = '01.11.1985'
 
 # Примеры некорректных дат
-date = '01.22.1001'
-date = '1.12.1001'
-date = '-2.10.3001'
-
+# date = '01.22.1001'
+# date = '1.12.1001'
+# date = '-2.10.3001'
+date = input('Введите дату в виде строки формата "dd.mm.yyyy": ')
+try:
+    dl = date.split('.')
+    if len(dl[0]) == 2 and len(dl[1]) == 2 and len(dl[2]) == 4:
+        if dl[0].isdigit() and dl[1].isdigit() and dl[2].isdigit():
+            dd = int(dl[0])
+            mm = int(dl[1])
+            yyyy = int(dl[2])
+            if 1 < yyyy <= 9999 and 1 < mm <= 12:
+                if (mm in (1, 3, 5, 7, 8, 10, 12) and 1 < dd <= 31) or (mm in (2, 4, 6, 9, 11) and 1 < dd <= 30):
+                    print('Дата выглядит корректно!')
+                else:
+                    print('Несоответствие чисел и месяцев!')
+            else:
+                print('Год меньше 1 или больше 9999!')
+        else:
+            print('Введенная дата содержит не только цифры!')
+    else:
+        print('Введенная дата не в виде строки формата "dd.mm.yyyy"!')
+except:
+    print('Ошибка при вводе даты!')
 
 # Задание-3: "Перевёрнутая башня" (Задача олимпиадного уровня)
 #
