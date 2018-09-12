daa85c8ea7d587b1cce43c4b459169a5bb78d947
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-13 01:09:52 +0300

    Update hw03_easy.py
    hw03easy completed, Petr O.

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,8 +4,27 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
-def my_round(number, ndigits):
-    pass
+def my_round(number, nd):
+    lnum = str(number).split('.')
+    if len(lnum[1]) > nd:
+        dr = int(lnum[1][0:nd])
+        ld = int(lnum[1][nd])
+        if ld >= 5:
+            dr += 1
+            snewdr = str(dr)
+        else:
+            snewdr = str(dr)
+
+        if len(snewdr) > nd:
+            sdi = str(int(lnum[0]) + 1)
+            snewdr = snewdr[1:]
+        else:
+            sdi = str(int(lnum[0]))
+
+        my_round = '{}.{}'.format(sdi, snewdr)
+    else:
+        my_round = str(number)
+    return my_round
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +39,22 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    ltn = str(ticket_number)
+    if len(ltn) == 6:
+        ls = 0
+        rs = 0
+        for li in ltn[0:3]:
+            ls = ls + int(li)
+        for li in ltn[3:6]:
+            rs = rs + int(li)
+        if ls == rs:
+            lucky_ticket = 'Lucky ticket! {} == {}'.format(ls, rs)
+        else:
+            lucky_ticket = 'Unlucky ticket! {} != {}'.format(ls, rs)
+    else:
+        lucky_ticket = 'Strange ticket! Not 6 digits!'
+    return lucky_ticket
+
 
 
 print(lucky_ticket(123006))
