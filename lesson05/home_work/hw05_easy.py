b062378e6bfb37699a2672e45434e6bad8d03f9b
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-19 20:20:09 +0300

    Update hw05_easy.py
    les05hweasy tasks1-3 completed, Petr O.

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -3,8 +3,44 @@
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
 
+#script1
+import os
+for i in range(9):
+    dir_i = 'dir_'+str(i+1)
+    dir_path = os.path.join(os.getcwd(), dir_i)
+    try:
+        os.mkdir(dir_path)
+    except FileExistsError:
+        print('Такая директория уже существует')
+
+#script2
+import os
+for i in range(9):
+    dir_i = 'dir_'+str(i+1)
+    dir_path = os.path.join(os.getcwd(), dir_i)
+    try:
+        print('директория удаляется: ', dir_i)
+        os.rmdir(dir_path) #- удаляет пустую директорию.
+    except Exception:
+       print('Такая директория не удаляется')
+
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+import os
+dir_path = os.path.join(os.getcwd())
+dirlist = [filename for filename in os.listdir(dir_path) if os.path.isdir(os.path.join(dir_path,filename))]
+print(dirlist)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+import os
+import shutil
+dir_path = os.getcwd()
+srcfile = os.path.realpath(__file__)
+dir_name = 'my_fcopy'
+destdir = dir_path + '\\' + dir_name
+try:
+    os.makedirs(dir_name)
+except Exception:
+    print('directory exists!')
+shutil.copy(srcfile, destdir)
