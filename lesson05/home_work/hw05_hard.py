b4b5dddb842b86cd6e08339666f1a4409e881896
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-21 14:58:52 +0300

    Update hw05_hard.py
    les05hwhard task1 completed, Petr O.

--- lesson05/home_work/hw05_hard.py
+++ lesson05/home_work/hw05_hard.py
@@ -18,13 +18,16 @@
 # python with_args.py param1 param2 param3
 import os
 import sys
-print('sys.argv = ', sys.argv)
-
+import shutil
 
 def print_help():
     print("help - получение справки")
     print("mkdir <dir_name> - создание директории")
     print("ping - тестовый ключ")
+    print("cp <file_name> - создает копию указанного файла")
+    print("rm <file_name> - удаляет указанный файл")
+    print("cd <full_path or relative_path> - меняет текущую директорию на указанную")
+    print("ls - отображение полного пути текущей директории")
 
 
 def make_dir():
@@ -42,14 +45,59 @@ def make_dir():
 def ping():
     print("pong")
 
+def cp_file():
+#   cp <file_name> - создает копию указанного файла
+    src = os.path.join(os.getcwd(), dir_name)
+    print(src)
+    try:
+        dst_dir = os.path.join(os.getcwd(), 'temp')
+        print(dst_dir)
+        shutil.copy2(src, dst_dir)
+    except Exception:
+        print('Ошибка при копировании!')
+
+def rm_file():
+#   rm <file_name> - удаляет указанный файл (запросить подтверждение операции)
+    print("Вы хотите удалить файл {}?".format(dir_name))
+    yes_no = input("Наберите 'y'-да, 'n'-нет: ")
+    if yes_no == 'y':
+        try:
+            os.remove(dir_name)
+        except Exception:
+            print('Ошибка при удалении!')
+
+def ch_dir():
+#   cd <full_path or relative_path> - меняет текущую директорию на указанную
+    if os.path.isabs(dir_name):
+        try:
+            os.chdir(dir_name)
+            print(dir_name)
+        except Exception:
+            print('Ошибка при переходе!')
+    else:
+        try:
+            os.chdir(os.path.join(os.getcwd(), dir_name))
+        except Exception:
+            print('Ошибка при переходе!')
+
+def ls_dir():
+#   ls - отображение полного пути текущей директории
+    print(os.path.abspath(os.getcwd()))
+
 do = {
     "help": print_help,
     "mkdir": make_dir,
-    "ping": ping
+    "ping": ping,
+    "cp": cp_file,
+    "rm": rm_file,
+    "cd": ch_dir,
+    "ls": ls_dir
+    
 }
 
 try:
     dir_name = sys.argv[2]
+    print(dir_name)
 except IndexError:
     dir_name = None
 
