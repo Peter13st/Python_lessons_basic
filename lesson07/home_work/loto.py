6c94f6f299c01ede3f2cc4cc0525c72d474aab10
peter13st <36024540+Peter13st@users.noreply.github.com>
2018-09-28 15:58:45 +0300

    Update loto.py
    les07hw Petr O.

--- lesson07/home_work/loto.py
+++ lesson07/home_work/loto.py
@@ -57,3 +57,112 @@
 модуль random: http://docs.python.org/3/library/random.html
 
 """
+class barrel:
+    #get barrels
+    def f(self):
+        lst = [x for x in range(1, self.amount + 1)]
+        random.shuffle(lst)
+        for i, y in enumerate(lst):
+            print('{:*^30}'.format('*'))
+            print('Вытащен бочонок: {} (осталось бочонков: {})'.format(y, self.amount - (i+1)))
+            #print(y)
+            yield y
+
+    def __init__(self, amount):
+        self.amount = amount
+        self.gen = self.f()
+
+
+
+class Lotto:
+    #prepare cards
+    def __set_card(self):
+        num = set()
+        while len(num) < self.all_row * 5:
+            num.add(random.randint(1, 91))
+        cards = list(num)
+        random.shuffle(cards)
+        
+        while len(cards) % self.all_row != 0:
+            cards.append('None')
+        self.all_row = int(len(cards) / self.all_row)
+        cards = [cards[i: i + self.all_row] for i in range(0,len(cards),self.all_row)]
+
+        for i in range(len(cards)):
+            cards[i].sort()
+        self.card_user = cards[:3]
+        self.card_comp = cards[3:]
+
+
+    def __init__(self, amount_card):
+        row = 3
+        self.all_row = row * amount_card
+        self.__set_card()
+
+
+    def get_card(self, card_player):
+        print('{:-^25}'.format(self.name))
+        print('{0[0]:>2} {0[1]:<10} {0[2]:<5} {0[3]} {0[4]} '.format(card_player[0]))
+        print('{0[0]:>4} {0[1]:<6} {0[2]:<4} {0[3]:<4} {0[4]} '.format(card_player[1]))
+        print('{0[0]} {0[1]:<5} {0[2]:<5} {0[3]:<5} {0[4]} '.format(card_player[2]))
+        print('{:-^25}'.format( '-'))
+
+    #Find number and winner
+    def search(self, card_player, num_barrel):
+        for i, n in enumerate(card_player):
+            if num_barrel in n:
+                card_player[i][n.index(num_barrel)] = '-'
+                self.score += 1
+                if self.score == 15:
+                    print('{} Победила!'.format(self.name))
+                    sys.exit(1)
+                return True
+        return False
+
+
+
+
+class Player(Lotto):
+
+    def __init__(self, name):
+        self.name = name
+        self.score = 0
+
+
+
+def main():
+
+    game = Lotto(2)
+    brl = barrel(90)
+    player1 = Player('Ваша карточка')
+    player2 = Player('Карточка компьютера')
+
+    while True:
+        num_barrel = next(brl.gen)
+        player1.get_card(game.card_user)
+        player2.get_card(game.card_comp)
+        
+        inp_user = input('Вычеркнуть цифру? (y/n)')
+        if inp_user == 'y':
+            if player1.search(game.card_user, num_barrel):
+                continue
+            else:
+                print('Конец игры')
+                sys.exit(1)
+        if inp_user == 'n':
+            if player1.search(game.card_user, num_barrel):
+                print('Конец игры')
+                sys.exit(1)
+            elif player2.search(game.card_comp, num_barrel):
+                continue
+        if inp_user != 'n' and inp_user != 'y':
+            print('Введите y or n')
+            #time.sleep(1)
+            continue
+
+    
+  
+
+
+if __name__ == '__main__':
+    main()
